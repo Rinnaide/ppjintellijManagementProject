e1183a00091c628088ace0fe597f6738bb9ba321
rinnaide <phencostarj@gmail.com>
2025-12-10 19:32:27 -0300

    awa

---
+++ backend/src/main/java/com/senac/projectmanagement/controller/WebController.java
@@ -0,0 +1,14 @@
+package com.senac.projectmanagement.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@CrossOrigin("*")
+@Controller
+public class WebController {
+    @GetMapping("/home")
+    public String home() {
+        return "forward:/index.html";
+    }
+}
