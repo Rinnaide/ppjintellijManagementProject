811103fd8e65fa092973210ca5ec7e06dd19b995
rinnaide <phencostarj@gmail.com>
2025-12-11 00:49:20 -0300

    webchange

--- backend/src/main/java/com/senac/projectmanagement/controller/WebController.java
+++ backend/src/main/java/com/senac/projectmanagement/controller/WebController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class WebController {
     @GetMapping("/home")
     public String home() {
-        return "forward:/index.html";
+        return "forward:/Web.html";
     }
 }
