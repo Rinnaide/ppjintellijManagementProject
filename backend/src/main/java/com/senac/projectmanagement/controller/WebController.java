e8aabbce9416602e7403226a0bc1e748e3bb4f59
rinnaide <phencostarj@gmail.com>
2025-12-11 00:58:35 -0300

    a

--- backend/src/main/java/com/senac/projectmanagement/controller/WebController.java
+++ backend/src/main/java/com/senac/projectmanagement/controller/WebController.java
@@ -1,17 +1,22 @@
 package com.senac.projectmanagement.controller;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.nio.file.Files;
+
 @CrossOrigin("*")
 @RestController
 public class WebController {
     @GetMapping(value = "/home", produces = MediaType.TEXT_HTML_VALUE)
-    public ResponseEntity<String> home() {
-        String htmlContent = "<!DOCTYPE html><html><head><title>Test</title></head><body><h1>Hello World from /home endpoint</h1></body></html>";
+    public ResponseEntity<String> home() throws IOException {
+        ClassPathResource resource = new ClassPathResource("static/Web.html");
+        String htmlContent = new String(Files.readAllBytes(resource.getFile().toPath()));
         return ResponseEntity.ok(htmlContent);
     }
 }
