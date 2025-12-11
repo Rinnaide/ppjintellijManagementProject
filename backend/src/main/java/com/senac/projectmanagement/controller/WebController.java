8fc71efed1dee7ba018abc9b8ed985ca7beb87b4
Pedro Costa <phencostarj@gmail.com>
2025-12-11 08:36:41 -0300

    Update WebController.java

--- backend/src/main/java/com/senac/projectmanagement/controller/WebController.java
+++ backend/src/main/java/com/senac/projectmanagement/controller/WebController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin("*")
-@RestController
+@Controller
 public class WebController {
     @GetMapping(value = "/home", produces = MediaType.TEXT_HTML_VALUE)
     public ResponseEntity<String> home() {
