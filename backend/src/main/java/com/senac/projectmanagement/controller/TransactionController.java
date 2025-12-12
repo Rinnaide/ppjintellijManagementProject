d76818fb0222c3c92e9983afdca22d91c8e1a56d
rinnaide <phencostarj@gmail.com>
2025-12-12 16:56:14 -0300

    Revert "aa"
    This reverts commit a73faf31452a66ed074b1f852d806a3f2d633e64.

--- backend/src/main/java/com/senac/projectmanagement/controller/TransactionController.java
+++ backend/src/main/java/com/senac/projectmanagement/controller/TransactionController.java
@@ -3,18 +3,14 @@ package com.senac.projectmanagement.controller;
 import com.senac.projectmanagement.dto.TransactionRequestDTO;
 import com.senac.projectmanagement.dto.TransactionResponseDTO;
 import com.senac.projectmanagement.entity.Transaction;
-import com.senac.projectmanagement.security.UserDetailsImpl;
 import com.senac.projectmanagement.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/transactions")
@@ -24,12 +20,7 @@ public class TransactionController {
     private TransactionService transactionService;
 
     @PostMapping
-    @PreAuthorize("isAuthenticated()")
-    public ResponseEntity<TransactionResponseDTO> createTransaction(@RequestBody TransactionRequestDTO transactionRequestDTO, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        // Check if the userId in the request matches the authenticated user or if the user is admin
-        if (!transactionRequestDTO.getUserId().equals(userDetails.getIdUsuario()) && !userDetails.getAuthorities().stream().anyMatch(auth -> auth.getAuthority().equals("ROLE_ADMINISTRATOR"))) {
-            return ResponseEntity.status(403).build(); // Forbidden
-        }
+    public ResponseEntity<TransactionResponseDTO> createTransaction(@RequestBody TransactionRequestDTO transactionRequestDTO) {
         Transaction createdTransaction = transactionService.createTransaction(transactionRequestDTO);
 
         // Convert Entity to DTO before returning
@@ -39,12 +30,7 @@ public class TransactionController {
     }
 
     @GetMapping("/user/{userId}")
-    @PreAuthorize("isAuthenticated()")
-    public ResponseEntity<List<TransactionResponseDTO>> getTransactionsByUser(@PathVariable Long userId, @RequestParam(defaultValue = "10") int limit, @RequestParam(defaultValue = "0") int offset, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        // Check if the userId matches the authenticated user or if the user is admin
-        if (!userId.equals(userDetails.getIdUsuario()) && !userDetails.getAuthorities().stream().anyMatch(auth -> auth.getAuthority().equals("ROLE_ADMINISTRATOR"))) {
-            return ResponseEntity.status(403).build(); // Forbidden
-        }
+    public ResponseEntity<List<TransactionResponseDTO>> getTransactionsByUser(@PathVariable Long userId, @RequestParam(defaultValue = "10") int limit, @RequestParam(defaultValue = "0") int offset) {
         List<Transaction> transactions = transactionService.getTransactionsByUser(userId, limit, offset);
         List<TransactionResponseDTO> response = transactions.stream()
                 .map(TransactionResponseDTO::new) // Chama o construtor para cada transação
@@ -53,12 +39,7 @@ public class TransactionController {
     }
 
     @GetMapping("/user/{userId}/date-range")
-    @PreAuthorize("isAuthenticated()")
-    public ResponseEntity<List<TransactionResponseDTO>> getTransactionsByUserAndDateRange(@PathVariable Long userId, @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate, @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        // Check if the userId matches the authenticated user or if the user is admin
-        if (!userId.equals(userDetails.getIdUsuario()) && !userDetails.getAuthorities().stream().anyMatch(auth -> auth.getAuthority().equals("ROLE_ADMINISTRATOR"))) {
-            return ResponseEntity.status(403).build(); // Forbidden
-        }
+    public ResponseEntity<List<TransactionResponseDTO>> getTransactionsByUserAndDateRange(@PathVariable Long userId, @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate, @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
         List<Transaction> transactions = transactionService.getTransactionsByUserAndDateRange(userId, startDate, endDate);
         List<TransactionResponseDTO> response = transactions.stream()
                 .map(TransactionResponseDTO::new)
@@ -67,33 +48,14 @@ public class TransactionController {
     }
 
     @GetMapping("/{id}")
-    @PreAuthorize("isAuthenticated()")
-    public ResponseEntity<TransactionResponseDTO> getTransactionById(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        Optional<Transaction> transactionOpt = transactionService.getTransactionById(id);
-        if (transactionOpt.isPresent()) {
-            Transaction transaction = transactionOpt.get();
-            // Check if the transaction belongs to the authenticated user or if the user is admin
-            if (!transaction.getUser().getUserId().equals(userDetails.getIdUsuario()) && !userDetails.getAuthorities().stream().anyMatch(auth -> auth.getAuthority().equals("ROLE_ADMINISTRATOR"))) {
-                return ResponseEntity.status(403).build(); // Forbidden
-            }
-            return ResponseEntity.ok(new TransactionResponseDTO(transaction));
-        }
-        return ResponseEntity.notFound().build();
+    public ResponseEntity<TransactionResponseDTO> getTransactionById(@PathVariable Long id) {
+        return transactionService.getTransactionById(id)
+                .map(transaction -> ResponseEntity.ok(new TransactionResponseDTO(transaction)))
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @PutMapping("/{id}")
-    @PreAuthorize("isAuthenticated()")
-    public ResponseEntity<TransactionResponseDTO> updateTransaction(@PathVariable Long id, @RequestBody TransactionRequestDTO transactionRequestDTO, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        // First, check if the transaction exists and belongs to the user
-        Optional<Transaction> transactionOpt = transactionService.getTransactionById(id);
-        if (transactionOpt.isPresent()) {
-            Transaction transaction = transactionOpt.get();
-            if (!transaction.getUser().getUserId().equals(userDetails.getIdUsuario()) && !userDetails.getAuthorities().stream().anyMatch(auth -> auth.getAuthority().equals("ROLE_ADMINISTRATOR"))) {
-                return ResponseEntity.status(403).build(); // Forbidden
-            }
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+    public ResponseEntity<TransactionResponseDTO> updateTransaction(@PathVariable Long id, @RequestBody TransactionRequestDTO transactionRequestDTO) {
         try {
             Transaction updatedTransaction = transactionService.updateTransaction(id, transactionRequestDTO);
             return ResponseEntity.ok(new TransactionResponseDTO(updatedTransaction));
@@ -103,18 +65,7 @@ public class TransactionController {
     }
 
     @DeleteMapping("/{id}")
-    @PreAuthorize("isAuthenticated()")
-    public ResponseEntity<Void> deleteTransaction(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        // First, check if the transaction exists and belongs to the user
-        Optional<Transaction> transactionOpt = transactionService.getTransactionById(id);
-        if (transactionOpt.isPresent()) {
-            Transaction transaction = transactionOpt.get();
-            if (!transaction.getUser().getUserId().equals(userDetails.getIdUsuario()) && !userDetails.getAuthorities().stream().anyMatch(auth -> auth.getAuthority().equals("ROLE_ADMINISTRATOR"))) {
-                return ResponseEntity.status(403).build(); // Forbidden
-            }
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+    public ResponseEntity<Void> deleteTransaction(@PathVariable Long id) {
         transactionService.deleteTransaction(id);
         return ResponseEntity.noContent().build();
     }
@@ -126,30 +77,19 @@ public class TransactionController {
     }
 
     @GetMapping("/user/{userId}/total-expense")
-    @PreAuthorize("isAuthenticated()")
-    public ResponseEntity<Double> getTotalExpense(@PathVariable Long userId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        // Check if the userId matches the authenticated user or if the user is admin
-        if (!userId.equals(userDetails.getIdUsuario()) && !userDetails.getAuthorities().stream().anyMatch(auth -> auth.getAuthority().equals("ROLE_ADMINISTRATOR"))) {
-            return ResponseEntity.status(403).build(); // Forbidden
-        }
+    public ResponseEntity<Double> getTotalExpense(@PathVariable Long userId) {
         Double totalExpense = transactionService.getTotalExpense(userId);
         return ResponseEntity.ok(totalExpense);
     }
 
     @GetMapping("/user/{userId}/filtered")
-    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<List<TransactionResponseDTO>> getFilteredTransactions(
             @PathVariable Long userId,
             @RequestParam(required = false) String searchQuery,
             @RequestParam(required = false) Long categoryId,
             @RequestParam(required = false) String type,
             @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
-            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate,
-            @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        // Check if the userId matches the authenticated user or if the user is admin
-        if (!userId.equals(userDetails.getIdUsuario()) && !userDetails.getAuthorities().stream().anyMatch(auth -> auth.getAuthority().equals("ROLE_ADMINISTRATOR"))) {
-            return ResponseEntity.status(403).build(); // Forbidden
-        }
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
         List<Transaction> transactions = transactionService.getFilteredTransactions(userId, searchQuery, categoryId, type, startDate, endDate);
         List<TransactionResponseDTO> response = transactions.stream()
                 .map(TransactionResponseDTO::new)
