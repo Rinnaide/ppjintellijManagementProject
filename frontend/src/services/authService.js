841dbcf949478aac042fcc8a700bec14f907b94b
rinnaide <phencostarj@gmail.com>
2025-12-12 17:18:20 -0300

    a

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,4 +1,5 @@
 import api, { setApiToken } from './api';
+import userService from './userService';
 
 const authService = {
   login: async (email, password) => {
@@ -6,7 +7,20 @@ const authService = {
       const response = await api.post('/auth/login', {"email": email ,"password":  password});
       const { usuario_id, usuario_nome, usuario_token } = response;
       setApiToken(usuario_token);
-      return { user: { id: usuario_id, name: usuario_nome }, token: usuario_token };
+
+      // Fetch full user details
+      const fullUserData = await userService.getUserById(usuario_id);
+
+      return {
+        user: {
+          id: usuario_id,
+          name: usuario_nome,
+          firstName: fullUserData.firstName,
+          lastName: fullUserData.lastName,
+          usuarioEmail: fullUserData.email
+        },
+        token: usuario_token
+      };
     } catch (error) {
       throw error;
     }
